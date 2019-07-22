62dad385927fefbf3b42c9ff37b462566e0a26b5
Claudia Mihaela Diosan <mcd1591@gmail.com>
2019-07-22 20:05:49 +0200

    nav bar colors changed and cities cards fixed

--- src/views/Cities.vue
+++ src/views/Cities.vue
@@ -3,18 +3,18 @@
 <v-container grid-list-lg >
     <v-layout row wrap>
         <v-flex xs4 v-for="i in city">
-          <v-card hover="true">
+          <v-card hover="true" color="#e8eaff">
             <v-card-media :src="i.img" height="200px">
             </v-card-media>
             <v-card-title primary-title class="justify-center">
               <div>
                 <h3 class="headline mb-0 ">{{i.name}}</h3>
-                <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+                <div>{{i.description}}</div>
               </div>
             </v-card-title>
             <v-card-actions class="justify-center">
-              <v-btn flat color="orange" @click="travelTo(i.lon, i.lat)">Travel</v-btn>
-              <v-btn flat color="orange">Explore</v-btn>
+              <v-btn flat color="#0000db" @click="travelTo(i.lon, i.lat)">Travel</v-btn>
+              <v-btn flat color="#f900b8">Explore</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -35,9 +35,9 @@ export default {
   data: function() {
     return {
       city: [
-        {img:'https://i.ibb.co/qFtLd56/lleida.jpg' , name: 'Lleida', lon:0.6419,lat:41.6109 },
-        {img:'https://i.ibb.co/zhwqZP4/barcelona.jpg',name: 'Barcelona', lon:2.154007, lat:41.390205},
-        {img:'https://i.ibb.co/LrK5xD4/newyork.jpg',name: 'New York', lon:-73.935242, lat:40.730610}
+        {img:'https://i.ibb.co/qFtLd56/lleida.jpg' , name: 'Lleida', lon:0.6419,lat:41.6109, description:'Is a city in the west of Catalonia, Spain. It is the capital city of the province of Lleida, with a populayion of 137,387 inhabitants in 2016. ' },
+        {img:'https://i.ibb.co/zhwqZP4/barcelona.jpg',name: 'Barcelona', lon:2.154007, lat:41.390205, description:'It is the capital and largest city of the autonomous community of Catalonia, with a population of 1.6 million within city limits.'},
+        {img:'https://i.ibb.co/LrK5xD4/newyork.jpg',name: 'New York', lon:-73.935242, lat:40.730610, description:' The City of New York, is the most populous city in the United States,with an estimated 2018 population of 8,398,748.'}
         ]
     }
 
