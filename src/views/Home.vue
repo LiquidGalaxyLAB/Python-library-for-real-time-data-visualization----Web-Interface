d3b50a94a4ae9c5a1c062eb361e361acb79b42c2
Claudia Mihaela Diosan <mcd1591@gmail.com>
2019-08-13 11:43:30 +0200

    home page fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-  <HelloWorld />
+  <v-container grid-list-lg >
+    <v-card
+      class="mx-auto"
+      color="#e8eaff"
+      max-width=fill-height
+    >
+
+    <v-card-title class="justify-center">
+      <span class="title font-weight-light align-center">A Google Summer Of Code 2019 Project</span>
+    </v-card-title>
+      <v-card-text class="headline font-weight">
+      This application's main function is the realtime data visualization through the Liquid Galaxy screens.<br>
+      </v-card-text>
+    </v-card>
+    </v-container>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
